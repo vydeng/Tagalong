0eb3f2d94ad5d05d5fd244c963ba5fd3839e0ed1
Sophia Lin <77797963+slin02@users.noreply.github.com>
2023-02-12 00:49:22 +0000

    added profile to user

--- frontend/src/app/academic-post/academic-post.component.ts
+++ frontend/src/app/academic-post/academic-post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AcademicService } from '../academic.service';
 import { Post } from '../post';
+import { LoginService } from '../login.service';
 
 
 @Component({
@@ -16,6 +17,7 @@ export class AcademicPostComponent {
   constructor(
     private formBuilder: FormBuilder,
     private academicService: AcademicService,
+    private loginService: LoginService,
   ) {}
 
   ngOnInit(){
@@ -32,7 +34,7 @@ export class AcademicPostComponent {
     if(this.form.invalid){
       return;
     }
-    this.academicService.addPost(new Post(this.form.value.title, this.form.value.time, this.form.value.people, this.form.value.description));
+    this.academicService.addPost(new Post(this.loginService.getProfile(), this.form.value.title, this.form.value.time, this.form.value.people, this.form.value.description));
     window.alert(`post submitted`);
     this.form.reset();
     this.submitted=false;
--- frontend/src/app/post.ts
+++ frontend/src/app/post.ts
@@ -1,10 +1,14 @@
+import { Profile } from "./profile";
+
 export class Post {
+    op: Profile;
     title: string;
     time: string;
     people: string;
     description: string;
 
-    constructor(title: string, time: string, people: string, description: string){
+    constructor(op: Profile, title: string, time: string, people: string, description: string){
+        this.op = op;
         this.title = title;
         this.time = time;
         this.people = people;
