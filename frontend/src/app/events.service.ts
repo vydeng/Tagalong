cdf99dc48d20cfdd3946bd5c1838db6875e5a5c8
Hayley <haowens@email.unc.edu>
2023-02-12 04:41:17 +0000

    message

--- frontend/src/app/events.service.ts
+++ frontend/src/app/events.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post'
+import { Profile } from './profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  eventsPosts: Post[] = [];
+  sam = new Profile("Sam", "contact", "bio"); 
+  bella = new Profile("Bella", "bellaswan@gmail.com", "i am from the pacific northwest");
+  // chad = new Profile("chad", "snap chad69_420", "in a frat");
+
+  post_one = new Post(this.sam, "anyone going to see Etran De L'Air play Cat's Cradle Backroom", "April 3rd 7pm", "everyone !", "anybody else planning on going to this show? i bought a ticket bc i love them but don't know anyone else going and want a buddy <3");
+  post_two = new Post(this.bella, "looking for 2 people to join for valentine's van gogh exhibit. i have a car and the tickets", "valentines @ 7", "2", "my throuple broke up yesterday and now i have 2 extra tickets to the Van Gogh valentine's exhibit in raleigh. come! it will be fun if we decide it will be. one person bring snacks one person bring drinks x");
+
+  eventsPosts: Post[] = [this.post_one, this.post_two];
   constructor() { }
 
   addPost(post: Post){
